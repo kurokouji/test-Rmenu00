be703e2f8d1a85c1d7168dc84f6461323dd4050c
SHIMOYAMA Yoshihiro <shimoyama@ergo.ne.jp>
2017-10-30 16:48:03 +0900

    レスポンス取得のため、修正したプログラムです。

--- System/Server/Libraries/Modules/RmenuLoggerMixin.rb
+++ System/Server/Libraries/Modules/RmenuLoggerMixin.rb
@@ -24,16 +24,36 @@ module RmenuLoggerMixin
     return log
   end
 
+  def createResponseLogger(file)
+    filename = $Rconfig["logs_path"] + "/" + file + ".log"  # ログファイル名の設定
+    log = Logger.new(filename, "daily")
+    
+    case $Rconfig["responselog_level"]                           # ログレベルの設定
+    when "FATAL"
+      log.level = Logger::FATAL
+    when "ERROR"
+      log.level = Logger::ERROR
+    when "WARN"
+      log.level = Logger::WARN
+    when "INFO"
+      log.level = Logger::INFO
+    else
+      log.level = Logger::DEBUG
+    end
+    
+    return log
+  end
+
   def changeLoggerMode(log, jsonData)
     htmlData = jsonData["html"]
-	  project  = htmlData.split("/")
-	
-	  # 開発支援プロジェクトはログを出力しない
-	  if project[0] == "RmenuVisualTools"
-	    log.level = Logger::FATAL
-	    return log
-	  end
-	
+    project  = htmlData.split("/")
+  
+    # 開発支援プロジェクトはログを出力しない
+    if project[0] == "RmenuVisualTools"
+      log.level = Logger::FATAL
+      return log
+    end
+  
     case $Rconfig["logger_level"]                           # ログレベルの設定
     when "FATAL"
       log.level = Logger::FATAL
@@ -50,6 +70,23 @@ module RmenuLoggerMixin
     return log
   end
 
+  # レスポンスログ　開始ログ取得
+  def responseLogStart(responseLog, rackName, remoteAddr, request_data)
+  
+    logdata = "remote address: " + remoteAddr + " program start: " + request_data["html"] + " mode: " + request_data["mode"]
+    responseLog.debug("#{rackName}") {"#{logdata}"}
+    
+  end
+  
+  # レスポンスログ　終了ログ取得
+  def responseLogEnd(responseLog, rackName, remoteAddr, response_data)
+  
+    logdata = "remote address: " + remoteAddr + " program   end: " + response_data["html"] + "mode: " + response_data["mode"]
+    responseLog.debug("#{rackName}") {"#{logdata}"}
+    
+  end
+  
+  
   # ファイル名（パス情報含む）を使用してHtmlファイルを取得
   def readHtml(fileName)
     begin
@@ -140,20 +177,20 @@ module RmenuLoggerMixin
   # パス（ディレクトリー）内の指定ディレクトリー以外を削除する
   def deleteDir(path, safedir)
     begin
-			searchdir = path + "/**"
-			
+      searchdir = path + "/**"
+      
       dirs = Dir.glob(searchdir)
       dirs.each {|readdir|
         if readdir == safedir
-			    next
-	      end
-	      FileUtils.rm_r(readdir)
+          next
+        end
+        FileUtils.rm_r(readdir)
       }
-			
+      
       return "OK"
     rescue Exception
       puts Exception.message
     end
-	end
-	
+  end
+  
 end
